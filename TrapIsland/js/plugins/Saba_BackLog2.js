c5827c4d755e877c7bd58b40e85d567ee85abb6d
RandomEnch <1048421623@qq.com>
2022-06-27 23:51:50 +0800

    index

--- TrapIsland/js/plugins/Saba_BackLog2.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    var _Game_Message_add = Game_Message.prototype.add;
-    Game_Message.prototype.add = function(text) {
-        Saba.BackLog.$gameBackLog.addLog('', text);
-        _Game_Message_add.apply(this, arguments);
-    };
-})();
